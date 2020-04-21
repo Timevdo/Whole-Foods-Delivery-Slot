28d15ea7cab117815a1f852a52f915b070d4cf79
Tim <tim.evdokimov@gmail.com>
2020-04-21 17:36:24 -0400

    Update

---
+++ config.py
@@ -0,0 +1,5 @@
+twilio_name = "ACc0d20a25e33859af31c1fc3f030e4a49"
+twilio_auth = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" #replace with own auth code
+
+my_number = "+1xxxxxxxxxx" #replace with own phone number
+
